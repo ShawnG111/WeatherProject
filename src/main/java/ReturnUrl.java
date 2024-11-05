095371f3b9b08a725ce4b2a2bdb4216c41cf2a6a
ShawnG111 <sahwn.guzheyuan@gmail.com>
2024-11-05 14:48:53 +0800

    .

---
+++ src/main/java/ReturnUrl.java
@@ -0,0 +1,22 @@
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class ReturnUrl {
+    public static String getUrl(String url){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).get().build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if(response.isSuccessful()){
+                String responseData = response.body().string();
+                return(responseData);
+            }else{
+                return("GET请求失败，响应码：" + response.code());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/excel/ExcelData.java
@@ -0,0 +1,39 @@
+package excel;
+
+public class ExcelData {
+    private String cityName;
+    private int adCode;
+    private int cityCode;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getAdCode() {
+        return adCode;
+    }
+
+    public void setAdCode(int adCode) {
+        this.adCode = adCode;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String toString() {
+        return "ExcelData{" +
+                "cityName='" + cityName + '\'' +
+                ", adCode=" + adCode +
+                ", cityCode=" + cityCode +
+                '}';
+    }
+}
