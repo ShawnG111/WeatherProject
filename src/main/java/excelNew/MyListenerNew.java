1b1463561a3c26db53dcc5be6ea97e009c4fa2e7
ShawnG111 <sahwn.guzheyuan@gmail.com>
2024-11-07 15:03:08 +0800

    .

--- src/main/java/excelNew/MyListenerNew.java
+++ src/main/java/excelNew/MyListenerNew.java
@@ -5,17 +5,27 @@ import com.alibaba.excel.read.listener.ReadListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MyListenerNew implements ReadListener<ExcelData> {
     private List<ExcelData> dataList = new ArrayList<>();
     private int i=1;
+
+    ExecutorService threadPool = Executors.newFixedThreadPool(4);
 
     public void invoke(ExcelData data, AnalysisContext context){
         dataList.add(data);
         if(dataList.size() >= 100){
             List<ExcelData> d = new ArrayList<>();
             d.addAll(dataList);
-            new Thread(new MyThreadNew(String.valueOf(i), d)).start();
+            threadPool.execute(new Runnable(){
+                public void run(){
+                    for(ExcelData excelData :d){
+                        CityJdbc.insert(excelData);
+                    }
+                }
+            });
             i++;
             dataList.clear();
         }
@@ -24,6 +34,12 @@ public class MyListenerNew implements ReadListener<ExcelData> {
     public void doAfterAllAnalysed(AnalysisContext context){
         List<ExcelData> d = new ArrayList<>();
         d.addAll(dataList);
-        new Thread(new MyThreadNew(String.valueOf(i), d)).start();
+        threadPool.execute(new Runnable(){
+            public void run(){
+                for(ExcelData excelData :d){
+                    CityJdbc.insert(excelData);
+                }
+            }
+        });
     }
 }
